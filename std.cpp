1783b451edfa6eb88ffeb2bc27e818020e58da36
kegalas <kaoshen@outlook.com>
2023-04-12 21:41:50 +0800

    update 2023-4-12 21:41

--- std.cpp
+++ std.cpp
@@ -12,20 +12,24 @@
 #include <cstdio>
 #include <bitset>
 #include <deque>
+#include <regex>
 
-#define LL long long
-#define ULL unsigned long long
-#define i128 __int128
 #define debug(a) std::cout<<#a<<"="<<a<<std::endl
 #define lth(i,x,y) for(int i=x;i<=y;i++)
 #define htl(i,x,y) for(int i=x;i>=y;i--)
 #define mms(x) memset(x, 0, sizeof(x))
 
-const int MAXN = 200005;
-const int INF = 0x7fffffff;
-const double EPS = 1e-8;
-const int MOD = 998244353;
-const double PI = acos(-1);
+using LL = long long;
+using ULL = unsigned long long;
+using i128 = __int128;
+using pii = std::pair<int,int>;
+using pll = std::pair<LL,LL>;
+
+int const MAXN = 200005;
+int const INF = 0x7fffffff;
+double const EPS = 1e-8;
+int const MOD = 998244353;
+double const PI = acos(-1);
 
 int arr[MAXN];
 
@@ -33,7 +37,7 @@ int main(){
     std::ios::sync_with_stdio(false);
     std::cin.tie(0);
 
-
+		
 
     return 0;
 }
