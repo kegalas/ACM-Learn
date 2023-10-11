246baaa93d8a8929f299f3ba8fcecf6cbfaacf77
kegalas <kaoshen@outlook.com>
2023-10-11 16:02:49 +0800

    update 2023-10-11 16:02

--- Algorithm/DP/maxSubArraySum/msas.cpp
+++ Algorithm/DP/maxSubArraySum/msas.cpp
@@ -1,3 +1,9 @@
+//luogu P1115
+//复杂度 n
+//求一个数组里面最大连续子段的和，可以为空，此时和为0
+//如果要保证非空，则可以在外部加一条
+//ans = std::max(ans, arr[i]);
+//并且如果每个数都是负数，则直接输出最大的负数。
 #include <iostream>
 #include <algorithm>
 #include <cmath>
@@ -13,7 +19,6 @@
 #include <bitset>
 #include <deque>
 #include <regex>
-#include <list>
 #include <unordered_map>
 #include <unordered_set>
 
@@ -41,14 +46,32 @@ DB const EPS = 1e-8;
 int const MOD = 998244353;
 DB const PI = acos(-1);
 
-int arr[MAXN];
+LL calc(std::vector<LL> const & vec){
+    LL ret=0;
+    LL cur=0;
+    for(int i=0;i<vec.size();i++){
+        cur += vec[i];
+        if(cur<0) cur = 0;
+        ret = std::max(ret, cur);
+    }
+    return ret;
+}
 
 void solve(){
     int n;
     std::cin>>n;
-    for(int i=1;i<=n;i++){
-        std::cin>>arr[i];
+    std::vector<LL> vec(n);
+    LL ans = -1e15;
+    for(int i=0;i<n;i++){
+        std::cin>>vec[i];
+        ans = std::max(ans,vec[i]);
     }
+    if(ans<0){
+        std::cout<<ans<<"\n";
+        return;
+    }
+    ans = std::max(ans, calc(vec));
+    std::cout<<ans<<"\n";
 }
 
 int main(){
@@ -56,8 +79,7 @@ int main(){
     std::cin.tie(0);
 
 	int T;
-	std::cin>>T;
-	//T=1;
+	T=1;
 	while(T--){
 	    solve();
 	}
