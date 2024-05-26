0ab64224e89299efa9dbe25e6f917221033c5f7d
kegalas <kaoshen@outlook.com>
2024-05-26 11:53:56 +0800

    update

--- Algorithm/computationalGeometry/nearestPoints/np.cpp
+++ Algorithm/computationalGeometry/nearestPoints/np.cpp
@@ -4,27 +4,46 @@
 #include <algorithm>
 #include <cmath>
 
-const int MAXN = 200005;
+typedef double db;
+typedef long double LD;
+db const EPS = 1e-9;
+int const MAXN = 100005;
+struct Point{db x,y;int id;}; // id为了记录最近点对是哪两个点
+typedef Point Vec;
 
-struct Node{
-    int x,y;
-    int id;
+bool eq(db a, db b)  {return std::abs(a - b)< EPS;}//等于
+bool ge(db a, db b)  {return a - b          > EPS;}//大于
+bool le(db a, db b)  {return a - b          < -EPS;}//小于
+bool geq(db a, db b) {return a - b          > -EPS;}//大于等于
+bool leq(db a, db b) {return a - b          < EPS;}//小于等于
+int sgn(db x) {
+    if (std::abs(x) < EPS) return 0;
+    if (x < 0) return -1;
+    return 1;
+} // 符号，等于零返回0，大于零返回1，小于零返回-1
 
-    bool operator<(const Node& a) const {
-        if(x!=a.x) return x<a.x;
-        return y<a.y;
-    }
-}nodes[MAXN];
+Vec operator+(Vec a, Vec b){return {a.x+b.x, a.y+b.y};}
+Vec operator-(Vec a, Vec b){return {a.x-b.x, a.y-b.y};}
+Vec operator*(db k, Vec v){return {k*v.x, k*v.y};}
+Vec operator*(Vec v, db k){return {v.x*k, v.y*k};}
+db operator*(Vec a, Vec b){return a.x*b.x+a.y*b.y;}
+db operator^(Vec a, Vec b){return a.x*b.y-a.y*b.x;}//叉积
+db len2(Vec v){return v.x*v.x+v.y*v.y;}//长度平方
+db len(Vec v){return std::sqrt(len2(v));}//向量长度
 
-bool cmp(const Node& a, const Node& b){
-    return a.y<b.y;
+bool operator<(Point a, Point b){return le(a.x,b.x)||(eq(a.x,b.x)&&le(a.y,b.y));}//横坐标第一关键字，纵坐标第二关键字
+
+bool cmp(Point const & a, Point const & b){
+    return le(a.y, b.y);
 }
 
-double minDist;
+db minDist;
 int ansA, ansB;
 
-inline void updAns(const Node& a, const Node& b){
-    double dist = sqrt((a.x-b.x)*(a.x-b.x)+(a.y-b.y)*(a.y-b.y)+0.0);
+Point po[MAXN];
+
+inline void updAns(Point const & a, Point const & b){
+    db dist = len(b-a);
     if(dist<minDist){
         minDist = dist;
         //如果要记录节点
@@ -37,29 +56,29 @@ void calcMin(int l, int r){
     if(r-l<=3){
         for(int i=l;i<=r;i++){
             for(int j=i+1;j<=r;j++){
-                updAns(nodes[i],nodes[j]);
+                updAns(po[i],po[j]);
             }
         }
-        std::sort(nodes+l, nodes+r+1, cmp);
+        std::sort(po+l, po+r+1, cmp); // 只排序y
         return;
     }
 
     int m = (l+r)>>1;
-    int midx = nodes[m].x;
+    db midx = po[m].x;
     calcMin(l,m);
     calcMin(m+1,r);
     
     //归并排序的合并，两个有序数组合并，合并之后仍然有序
-    std::inplace_merge(nodes+l, nodes+m+1, nodes+r+1, cmp);
+    std::inplace_merge(po+l, po+m+1, po+r+1, cmp); // 只排序y
 
-    static Node t[MAXN];
+    static Point t[MAXN];
     int tsz = 0;
 
     for (int i = l; i <= r; ++i){
-        if (abs(nodes[i].x - midx) < minDist) {
-            for (int j = tsz - 1; j >= 0 && nodes[i].y - t[j].y < minDist; --j)
-                updAns(nodes[i], t[j]);
-            t[tsz++] = nodes[i];
+        if (le(std::abs(po[i].x - midx),minDist)) {
+            for (int j = tsz - 1; j >= 0 && le(po[i].y - t[j].y,minDist); --j)
+                updAns(po[i], t[j]);
+            t[tsz++] = po[i];
         }
     }
 
@@ -69,15 +88,17 @@ int main(){
     int n;
     std::cin>>n;
     for(int i=1;i<=n;i++){
-        std::cin>>nodes[i].x>>nodes[i].y;
-        nodes[i].id = i;
+        std::cin>>po[i].x>>po[i].y;
+        po[i].id = i;
     }
 
-    std::sort(nodes+1,nodes+1+n);
+    std::sort(po+1,po+1+n); // 按照x第一，y第二排序
     minDist = 1e20;
     calcMin(1,n);
 
-    printf("%.4f\n",minDist);
+    std::cout<<std::fixed;
+    std::cout.precision(4);
+    std::cout<<minDist<<"\n";
 
     return 0;
 }
--- Algorithm/computationalGeometry/二维数点/二维数点.cpp
+++ Algorithm/computationalGeometry/二维数点/二维数点.cpp
@@ -1,86 +1,85 @@
-//Luogu P2163
-//时间复杂度 nlogn
-#include <iostream>
+// Luogu P2163
+// 时间复杂度 nlogn
+// 给定平面上n个点，m次查询，查询一个矩阵内有多少点
+// 由于坐标范围很大，不能直接用前缀和
 #include <algorithm>
+#include <iostream>
 
 int const MAXN = 500005;
 
-struct Point{
-    int x,y;
-    int tag;//用于区分实际的点和查询时的虚点  
-    
-    bool operator<(Point const & p){
-        if(x!=p.x) return x<p.x;
-        if(y!=p.y) return y<p.y;
-        return tag<p.tag;
-    }
-}pts[MAXN*5];//实点和查询矩阵的点都放在这里面
+struct Point {
+    int x, y;
+    int tag;  // 用于区分实际的点和查询时的虚点
 
-int b[MAXN*5];
+    bool operator<(Point const& p) {
+        if (x != p.x) return x < p.x;
+        if (y != p.y) return y < p.y;
+        return tag < p.tag;
+    }
+} pts[MAXN * 5];  // 实点和查询矩阵的点都放在这里面
+
+int b[MAXN * 5];
 int bit[MAXN];
 int ans[MAXN][5];
 int tot[MAXN];
 
-inline int lowbit(int n){
-    return n&(-n);
-}
+inline int lowbit(int n) { return n & (-n); }
 
-void update(int p, int k, int n){
-    for(;p<=n;p+=lowbit(p)){
-        bit[p]+=k;
+void update(int p, int k, int n) {
+    for (; p <= n; p += lowbit(p)) {
+        bit[p] += k;
     }
 }
 
-int query(int p){
-    int ret=0;
-    for(;p;p-=lowbit(p)){
-        ret+=bit[p];
+int query(int p) {
+    int ret = 0;
+    for (; p; p -= lowbit(p)) {
+        ret += bit[p];
     }
     return ret;
 }
 
-inline int lsh(int x, int cnt){//离散化函数
-    return std::lower_bound(b+1,b+cnt+1,x)-b;
+inline int lsh(int x, int cnt) {  // 离散化函数
+    return std::lower_bound(b + 1, b + cnt + 1, x) - b;
 }
 
-int main(){
+int main() {
     std::ios::sync_with_stdio(false);
     std::cin.tie(0);
 
-	int n,m;
-	std::cin>>n>>m;//点数，查询数
+    int n, m;
+    std::cin >> n >> m;  // 点数，查询数
 
-    for(int i=1;i<=n;i++){
-        std::cin>>pts[i].x>>pts[i].y;//所有实点的坐标
+    for (int i = 1; i <= n; i++) {
+        std::cin >> pts[i].x >> pts[i].y;  // 所有实点的坐标
         pts[i].tag = 0;
     }
-    
-    for(int i=1;i<=m;i++){
-        int x1,x2,y1,y2;
-        std::cin>>x1>>y1>>x2>>y2;//查询的长方形的左下角和右上角
-        
-        pts[++n].x = x1-1, pts[n].y = y1-1, pts[n].tag = i;
+
+    for (int i = 1; i <= m; i++) {
+        int x1, x2, y1, y2;
+        std::cin >> x1 >> y1 >> x2 >> y2;  // 查询的长方形的左下角和右上角
+
+        pts[++n].x = x1 - 1, pts[n].y = y1 - 1, pts[n].tag = i;
         pts[++n].x = x2, pts[n].y = y2, pts[n].tag = i;
-        pts[++n].x = x2, pts[n].y = y1-1, pts[n].tag = i;
-        pts[++n].x = x1-1, pts[n].y = y2, pts[n].tag = i;
+        pts[++n].x = x2, pts[n].y = y1 - 1, pts[n].tag = i;
+        pts[++n].x = x1 - 1, pts[n].y = y2, pts[n].tag = i;
     }
-    
-    std::sort(pts+1,pts+1+n);
-    for(int i=1;i<=n;i++) b[i] = pts[i].y;
-    std::sort(b+1,b+1+n);
-    int cnt = std::unique(b+1,b+1+n) - b - 1;//把所有y离散化
-    
-    for(int i=1;i<=n;i++){
-        if(pts[i].tag){
+
+    std::sort(pts + 1, pts + 1 + n);
+    for (int i = 1; i <= n; i++) b[i] = pts[i].y;
+    std::sort(b + 1, b + 1 + n);
+    int cnt = std::unique(b + 1, b + 1 + n) - b - 1;  // 把所有y离散化
+
+    for (int i = 1; i <= n; i++) {
+        if (pts[i].tag) {
             ans[pts[i].tag][++tot[pts[i].tag]] = query(lsh(pts[i].y, cnt));
-        }
-        else{
+        } else {
             update(lsh(pts[i].y, cnt), 1, cnt);
         }
     }
-    
-    for(int i=1;i<=m;i++){
-        std::cout<<ans[i][4]-ans[i][3]-ans[i][2]+ans[i][1]<<"\n";
+
+    for (int i = 1; i <= m; i++) {
+        std::cout << ans[i][4] - ans[i][3] - ans[i][2] + ans[i][1] << "\n";
     }
 
     return 0;
